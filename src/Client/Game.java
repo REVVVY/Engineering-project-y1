ecc14e7a1d0ae64150d2460cdd1a5e760358aa80
REVVVY <39383352+REVVVY@users.noreply.github.com>
2021-04-19 10:53:27 +0200

    Add files via upload

---
+++ src/Client/Game.java
@@ -0,0 +1,26 @@
+package Client;
+
+public class Game {
+
+    private Player player1;
+    private Player player2;
+
+    public Game(Player player1){
+        this.player1 = player1;
+    }
+
+    public Game(Player player1, Player player2){
+        this.player1 = player1;
+        this.player2 = player2;
+    }
+
+    public Player getPlayer1() {
+        return player1;
+    }
+
+    public Player getPlayer2() {
+        return player2;
+    }
+
+    
+}
---
+++ src/Server/StartServer.java
@@ -0,0 +1,10 @@
+package Server;
+
+/***
+ * Main klass för att starta servern
+ */
+public class StartServer {
+    public static void main(String[] args) {
+        Server server = new Server(2020);
+    }
+}
